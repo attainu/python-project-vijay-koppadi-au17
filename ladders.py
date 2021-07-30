95c4a6c385859be6cbb8359981366a78b546dc76
vijaykoppadi <82393087+vijay-koppadi-au17@users.noreply.github.com>
2021-07-30 13:07:00 -0700

    Add files via upload

---
+++ ladders.py
@@ -0,0 +1,9 @@
+def ldr():
+    ladder1=37
+    ladder2=10
+    ladder3=22
+    ladder4=21
+    ladder5=56
+    ladder6=16
+    ladder7=19
+    ladder8=19
---
+++ snakes.py
@@ -0,0 +1,9 @@
+def snake():
+    snake1=10
+    snake2=20
+    snake3=20
+    snake4=4
+    snake5=42
+    snake6=19
+    snake7=20
+    snake8=19
